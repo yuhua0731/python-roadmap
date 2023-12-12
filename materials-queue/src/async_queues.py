bad8176d9c9475676cbc7bb5f5ea818b2b4ec29e
yuhua <yuhua364@gmail.com>
2023-12-12 10:59:53 +0800

    adjust link visit strategy, avoiding redundant operations and incorrect count

--- materials-queue/src/async_queues.py
+++ materials-queue/src/async_queues.py
@@ -24,6 +24,7 @@ async def main(args):
     session = aiohttp.ClientSession()
     try:
         links = Counter()
+        # uncomment one of the following lines to use a different queue
         queue = asyncio.Queue()
         # queue = asyncio.LifoQueue()
         # queue = asyncio.PriorityQueue()
@@ -56,13 +57,24 @@ async def main(args):
 async def worker(worker_id, session, queue, links, max_depth):
     print(f"[{worker_id} starting]", file=sys.stderr)
     while True:
+        # 获取到一个url，计数+1
+        # 使用fetch_html以及parse_links获取到该url下的所有链接
+        # 并将这些链接放入queue中，depth+1
         url, depth = await queue.get()
         links[url] += 1
+        print(f"[{worker_id} {depth=} {url=} {links[url]}]")
         try:
-            if depth <= max_depth:
-                print(f"[{worker_id} {depth=} {url=}]", file=sys.stderr)
+            # 以下实现方式会将depth大于max_depth的url也放入queue中，增加无谓的put & get操作，也会导致links[url]的计数不准确
+            # if depth <= max_depth:
+            #     print(f"[{worker_id} {depth=} {url=}]", file=sys.stderr)
+            #     if html := await fetch_html(session, url):
+            #         for link_url in parse_links(url, html):
+            #             await queue.put(Job(link_url, depth + 1))
+            
+            if depth < max_depth: # depth + 1 <= max_depth
                 if html := await fetch_html(session, url):
                     for link_url in parse_links(url, html):
+                        print(f'{url} -> {link_url}')
                         await queue.put(Job(link_url, depth + 1))
         except aiohttp.ClientError:
             print(f"[{worker_id} failed at {url=}]", file=sys.stderr)
@@ -83,7 +95,7 @@ def parse_links(url, html):
         if not href.startswith("javascript:"):
             yield urljoin(url, href)
 
-
+# an elegent way to parse command line arguments
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("url")
@@ -93,9 +105,17 @@ def parse_args():
 
 
 def display(links):
+    # Counter.most_common() returns n most common elements in the counter, if None, return all elements
     for url, count in links.most_common():
         print(f"{count:>3} {url}")
 
+# TEST
+async def test():
+    await asyncio.sleep(1)
+    print('hello')
+    await asyncio.sleep(1)
+    print('world')
 
 if __name__ == "__main__":
+    # asyncio.run(test())
     asyncio.run(main(parse_args()))
