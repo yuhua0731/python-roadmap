0f6912598dfae9bd4804715d020f19c3aa3282cb
yuhua <yuhua364@gmail.com>
2023-12-28 14:37:50 +0800

    [learning] modify multiprocess_queue.py and change the r/w permission of some scripts

--- materials-queue/src/multiprocess_queue.py
+++ materials-queue/src/multiprocess_queue.py
@@ -8,6 +8,10 @@ from dataclasses import dataclass
 from hashlib import md5
 from string import ascii_lowercase
 
+from itertools import product
+
+# Choosing the value for a sentinel can be tricky, especially with the multiprocessing module because of how it handles the global namespace.
+# It’s probably safest to stick to a predefined value such as None, which has a known identity everywhere
 POISON_PILL = None
 
 
@@ -40,6 +44,8 @@ class Job:
         for index in range(self.start_index, self.stop_index):
             text_bytes = self.combinations[index].encode("utf-8")
             hashed = md5(text_bytes).hexdigest()
+            # 如果我们找到了一个匹配的散列值，我们将其返回
+            # 否则，我们将返回None
             if hashed == hash_value:
                 return text_bytes.decode("utf-8")
 
@@ -55,9 +61,11 @@ class Worker(multiprocessing.Process):
         while True:
             job = self.queue_in.get()
             if job is POISON_PILL:
+                # 如果我们收到毒丸，我们将其放回队列并退出, 以便其他Worker也可以退出
                 self.queue_in.put(POISON_PILL)
                 break
             if plaintext := job(self.hash_value):
+                # 如果job返回值不是None，则表示我们找到了一个匹配的反编码组合，我们将其放入输出队列
                 self.queue_out.put(plaintext)
                 break
 
@@ -81,6 +89,7 @@ def main(args):
         for indices in chunk_indices(len(combinations), len(workers)):
             queue_in.put(Job(combinations, *indices))
 
+    # use POISON_PILL to signal the end of the queue
     queue_in.put(POISON_PILL)
 
     while any(worker.is_alive() for worker in workers):
@@ -96,6 +105,13 @@ def main(args):
         print("Unable to find a solution")
 
 
+def main_single(args):
+    # single thread
+    t1 = time.perf_counter()
+    text = reverse_md5(args.hash_value, max_length=args.max_length)
+    print(f"{text} (found in {time.perf_counter() - t1:.1f}s)")
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("hash_value")
@@ -106,10 +122,20 @@ def parse_args():
         type=int,
         default=multiprocessing.cpu_count(),
     )
+    # default workers will be set to the number of cores on the machine
     return parser.parse_args()
 
 
 def chunk_indices(length, num_chunks):
+    """split length into num_chunks, evenly
+
+    Args:
+        length (int): number of items to be split
+        num_chunks (int): number of chunks to split length into
+
+    Yields:
+        start, end pairs (int, int): start and stop indices of each chunk
+    """
     start = 0
     while num_chunks > 0:
         num_chunks = min(num_chunks, length)
@@ -119,5 +145,21 @@ def chunk_indices(length, num_chunks):
         num_chunks -= 1
 
 
+def reverse_md5(hash_value, alphabet=ascii_lowercase, max_length=6):
+    for length in range(1, max_length + 1):
+        for combination in product(alphabet, repeat=length):
+            text_bytes = "".join(combination).encode("utf-8")
+            hashed = md5(text_bytes).hexdigest()
+            if hashed == hash_value:
+                return text_bytes.decode("utf-8")
+
+
 if __name__ == "__main__":
+    for start, stop in chunk_indices(20, 6):
+        print(len(r := range(start, stop)), r)
+
+    # single thread
+    main_single(parse_args())
+    
+    # multi process
     main(parse_args())
